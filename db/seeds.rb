a224a9e72180856302e174b8205d83519660a350
ladref <louis.dreyfus@icloud.com>
2019-08-02 00:28:42 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creation of 10 cities
+10.times do
+  city = Faker::Address.city
+  City.create(city: city)
+end
+
+# Creation of the 30 dog_sitters
+30.times do
+  name = Faker::Name.name
+  city_id = rand(1..10)
+  DogSitter.create(name: name, city_id: city_id)
+end
+
+# Creation of 100 dogs
+100.times do
+  name = Faker::Name.name
+  city_id = rand(1..10)
+  breed = Faker::Superhero.name
+  Dog.create(name: name, city_id: city_id, breed: breed)
+end
+
+# Creation of 200 strolls
+200.times do
+  dog_id = rand(1..100)
+  dog_sitter_id = rand(1..30)
+  Strolls.create(dog_id: dog_id, dog_sitter_id: dog_sitter_id)
+end
